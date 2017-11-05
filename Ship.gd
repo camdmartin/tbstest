0401acd1c732cb4c668e55079bc81116cd1e12d6
Cam Martin <cameron.d.martin@gmail.com>
2017-11-05 18:54:21 -0500

    fixed selection bug, added deselection

--- Ship.gd
+++ Ship.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 var selected = false
+const BATCH_SELECT_SIZE = 5
 
 func _ready():
 	set_process_input(true)
@@ -12,20 +13,42 @@ func _input_event(viewport, event, shape_idx):
 			ships_on_planet.append(s)
 	
 	if ships_on_planet.size() > 0:
-		if event.type == InputEvent.MOUSE_BUTTON and event.button_index == BUTTON_LEFT and Input.is_action_pressed("single_select"):
+		if event.type == InputEvent.MOUSE_BUTTON and event.button_index == BUTTON_LEFT and event.is_pressed() and Input.is_action_pressed("single_select"):
 			# CTRL-click for single ships
 			ships_on_planet[0].selected = true
-			print("selected")
-		elif event.type == InputEvent.MOUSE_BUTTON and event.button_index == BUTTON_LEFT and Input.is_action_pressed("batch_select"):
-			# SHIFT-click for groups of 5
-			if ships_on_planet.size() > 5:
-				for i in range(4):
+		elif event.type == InputEvent.MOUSE_BUTTON and event.button_index == BUTTON_LEFT and event.is_pressed() and Input.is_action_pressed("batch_select"):
+			# SHIFT-click for batches of specified size
+			if ships_on_planet.size() > BATCH_SELECT_SIZE:
+				for i in range(BATCH_SELECT_SIZE):
 					ships_on_planet[i].selected = true
 			else:
 				for s in ships_on_planet:
 					s.selected = true
-		elif event.type == InputEvent.MOUSE_BUTTON and event.button_index == BUTTON_LEFT and event.pressed:
+		elif event.type == InputEvent.MOUSE_BUTTON and event.button_index == BUTTON_LEFT and event.is_pressed():
 			# left click selects all ships
 			for s in ships_on_planet:
 				s.selected = true
+	# deselection
+	var ships_on_planet = []
+	for s in self.get_parent().get_ships():
+		if s.selected == true:
+			ships_on_planet.append(s)
+	
+	if ships_on_planet.size() > 0:
+		if event.type == InputEvent.MOUSE_BUTTON and event.button_index == BUTTON_RIGHT and event.is_pressed() and Input.is_action_pressed("single_select"):
+			# CTRL-click for single ships
+			ships_on_planet[0].selected = false
+		elif event.type == InputEvent.MOUSE_BUTTON and event.button_index == BUTTON_RIGHT and event.is_pressed() and Input.is_action_pressed("batch_select"):
+			# SHIFT-click for batches of specified size
+			if ships_on_planet.size() > BATCH_SELECT_SIZE:
+				for i in range(BATCH_SELECT_SIZE):
+					ships_on_planet[i].selected = false
+			else:
+				for s in ships_on_planet:
+					s.selected = false
+		elif event.type == InputEvent.MOUSE_BUTTON and event.button_index == BUTTON_RIGHT and event.is_pressed():
+			# right click deselects all ships
+			for s in ships_on_planet:
+				s.selected = false
+				
 	self.get_parent().update_ship_display()
